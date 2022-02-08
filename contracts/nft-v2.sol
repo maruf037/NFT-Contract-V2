2ebee0a0e8c18205ca8af7489b23cdc70941d23c
md-maruf-hossain <sany.maruf@gmail.com>
2022-02-08 14:54:21 +0600

    updated hardhat configuration

--- contracts/nft-v2.sol
+++ contracts/nft-v2.sol
@@ -3,10 +3,10 @@
 
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import '@openzeppelin/contracts/utils/Counters.sol';
-import '@openzeppelin/contracts/token/access/Ownable.sol';
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract nftv2 is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
@@ -17,7 +17,7 @@ contract nftv2 is ERC721URIStorage, Ownable {
     function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns(uint256) {
         _tokenIds.increment();
 
-        uint256 newItermId = _tokenIds.current();
+        uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
